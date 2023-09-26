e409e5b05e1b5b329b48793686cb8c3f93eb5ff1
publdaze <publdaze@naver.com>
2023-09-26 16:48:40 +0900

    Feat: prettier 적용
    #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-  return <div className="App">위키키</div>;
+  return <div className='App'>위키키</div>;
 }
 
 export default App;
