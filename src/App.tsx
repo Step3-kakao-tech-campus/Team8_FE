640fb278890bd312ee2627e5b3d14c2df4bfa8b5
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-10-01 23:12:35 +0900

    Fix: 중복 import 해결
    - 중복되는 Outlet import 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@material-tailwind/react';
-import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
