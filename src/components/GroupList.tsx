48c7262732620fd00295e0db661e7a3666d7fcd6
Song Jihye <qwer15948@pusan.ac.kr>
2023-10-01 23:06:23 +0900

    Refactor: 함수 형태 수정
    - return 및 중괄호 생략
    - 형식 통일

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -10,9 +10,9 @@ interface Group {
 const GroupList = ({ groups }: { groups: Group[] }) => {
   return (
     <div className='grid grid-cols-4 gap-4 p-0'>
-      {groups.map((item) => {
-        return <GroupCard key={item.groupId} group={item} />;
-      })}
+      {groups.map((item) => (
+        <GroupCard key={item.groupId} group={item} />
+      ))}
     </div>
   );
 };
