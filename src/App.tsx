9659fa7cfcd3de04e4d7c93956d7f1b60c733b25
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-10-01 14:40:40 +0900

    Feat: 푸터 구현
    #20

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@material-tailwind/react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,6 +21,7 @@ const App = () => {
         <ThemeProvider>
           <Header />
           <Outlet />
+          <Footer />
         </ThemeProvider>
       </QueryClientProvider>
     </RecoilRoot>
