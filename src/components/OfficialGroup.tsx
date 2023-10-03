adcde3aa9eb87998c0a670a0df966332190cf6c6
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-10-03 17:39:48 +0900

    Fix: key 값 추가
    - key WARNING 해결

--- src/components/OfficialGroup.tsx
+++ src/components/OfficialGroup.tsx
@@ -24,7 +24,7 @@ const OfficialGroup = () => {
   return (
     <Carousel className='rounded-xl max-w-[768px]' loop>
       {dummy.map((group) => (
-        <div className='relative h-full w-full'>
+        <div key={group.groupId} className='relative h-full w-full'>
           <img src={group.groupImage} alt={group.groupName} className='w-full h-40 object-cover mx-auto' />
           <div className='absolute inset-0 grid h-full w-full place-items-center bg-black/50'>
             <div className='w-3/4 text-center'>
