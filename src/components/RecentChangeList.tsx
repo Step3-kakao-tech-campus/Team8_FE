014d086370305a8d070dfef7e3c9e5940408141f
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-10-05 22:53:22 +0900

    Design: 폰트 크기 수정
    - 전체적인 폰트 크기 축소
    
    #39 #45

--- src/components/RecentChangeList.tsx
+++ src/components/RecentChangeList.tsx
@@ -9,7 +9,7 @@ const RecentChangeList = () => {
 
   return (
     <div className='w-44'>
-      <h2 className='text-lg font-bold px-1 py-2'>최근 변경된 페이지</h2>
+      <h2 className='font-bold px-1 py-2'>최근 변경된 페이지</h2>
       <ul className='p-4 bg-gray-100 text-sm'>
         {dummy.map((change) => (
           <li key={change.pageId} className='my-2 leading-tight'>
