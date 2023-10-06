40d0bfc2000261d43fdb40e34df2f53fd33a11b4
publdaze <publdaze@naver.com>
2023-10-07 03:07:38 +0900

    Feat: 그룹 생성 페이지 틀 생성
    #53

--- src/pages/GroupCreatePage.tsx
+++ src/pages/GroupCreatePage.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const GroupCreatePage = () => {
-  return <div />;
+  return (
+    <div className='max-w-3xl min-w-max m-auto flex flex-col min-h-screen py-60'>
+      <section />
+    </div>
+  );
 };
 
 export default GroupCreatePage;
