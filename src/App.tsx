e4411577cd8db55bb031cf3e4757688d1200eec4
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-10-08 12:56:35 +0900

    Style: craco 관련 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@material-tailwind/react';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const queryClient = new QueryClient({
   defaultOptions: {
