d78050ea5a94f68a11450040259c5be5d09d8cbd
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-10-10 11:56:17 +0900

    Feat: 초대 링크 컴포넌트 수정
    - 클립보드 복사 기능 추가
    - 글꼴 변경
    
    #65

--- src/components/GroupCreateCompleteSection.tsx
+++ src/components/GroupCreateCompleteSection.tsx
@@ -5,17 +5,36 @@ import { MdContentCopy } from 'react-icons/md';
 const GroupCreateCompleteSection = () => {
   const inviteCode = 'https://www.dfdasdf.ffdda.com/ffaefdfdasdf';
 
+  const handleCopyClipBoard = (text: string) => {
+    try {
+      navigator.clipboard.writeText(text);
+      alert('클립보드에 복사되었습니다.');
+    } catch (error) {
+      alert('클립보드 복사에 실패하였습니다.');
+    }
+  };
   return (
     <section className='space-y-10'>
       <div>
-        <Typography variant='lead'>
+        <Typography variant='lead' className='font-nanum'>
           그룹 생성이 <strong>완료</strong>되었습니다!
         </Typography>
-        <Typography variant='paragraph'>초대 링크를 통해 그룹원을 초대해보세요.</Typography>
+        <Typography variant='paragraph' className='font-nanum'>
+          초대 링크를 통해 그룹원을 초대해보세요.
+        </Typography>
       </div>
-      <Input className='truncate' value={inviteCode} size='lg' readOnly crossOrigin='' icon={<MdContentCopy />} />
+      <Input
+        className='truncate outline-none font-nanum'
+        label='초대 링크'
+        value={inviteCode}
+        size='lg'
+        readOnly
+        crossOrigin=''
+        icon={<MdContentCopy onClick={() => handleCopyClipBoard(inviteCode)} />}
+        onClick={() => handleCopyClipBoard(inviteCode)}
+      />
       <div className='flex justify-end'>
-        <Button>시작하기</Button>
+        <Button className='font-nanum'>시작하기</Button>
       </div>
     </section>
   );
