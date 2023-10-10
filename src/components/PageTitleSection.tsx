3c150aa371b4ebba9cb4606880d086fc327d8024
publdaze <publdaze@naver.com>
2023-10-10 17:35:11 +0900

    Feat: 제목 섹션 컴포넌트 생성
    #68

---
+++ src/components/PageTitleSection.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface PageTitleSectionProps {
+  title: string;
+  aboveAdornment?: React.ReactNode;
+  underAdornment?: React.ReactNode;
+}
+
+const PageTitleSection = ({ title, aboveAdornment, underAdornment }: PageTitleSectionProps) => {
+  return (
+    <section className='w-full flex flex-col justify-center max-w-full px-8'>
+      <div className='mx-20'>
+        <div className='px-2 border-b-2 flex mb-2'>
+          <h1 className='text-4xl leading-normal font-semibold text-start'>{title}</h1>
+          {aboveAdornment}
+        </div>
+        {underAdornment}
+      </div>
+    </section>
+  );
+};
+
+export default PageTitleSection;
