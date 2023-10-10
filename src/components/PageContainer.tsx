6967f22f3f3842724390e4f3b093bb2bac041d82
publdaze <publdaze@naver.com>
2023-10-10 17:44:39 +0900

    Feat: 페이지 컨테이너에 최근 변경된 페이지 컴포넌트 삽입
    - 최근 변경된 페이지 우측 column에 삽입
    - 스크롤 내릴 시 짤림 방지를 위해 top 조정
    
    #68

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RecentChangeList from './RecentChangeList';
 
 interface PageContainerProps {
   children: React.ReactNode;
@@ -7,9 +8,11 @@ interface PageContainerProps {
 const PageContainer = ({ children }: PageContainerProps) => {
   return (
     <div className='mx-auto flex w-full max-w-full flex-col xl:flex-row items-start justify-center gap-4 xl:gap-6 2xl:gap-10 px-8 py-10'>
-      <aside className='sticky w-full top-8 xl:w-52 2xl:w-64 shrink-0'>{/* 목차 */}</aside>
+      <aside className='sticky w-full top-20 xl:w-52 2xl:w-64 shrink-0'>{/* 목차 */}</aside>
       <main className='w-full xl:max-w-4xl'>{children}</main>
-      <aside className='sticky top-8 w-full xl:w-52 2xl:w-64 shrink-0'>{/* 최근 변경 */}</aside>
+      <aside className='sticky top-20 w-full xl:w-52 2xl:w-64 shrink-0'>
+        <RecentChangeList />
+      </aside>
     </div>
   );
 };
