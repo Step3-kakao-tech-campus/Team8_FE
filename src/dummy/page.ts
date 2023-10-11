9828e6a4ede49d4cd51ae0c4ca5fda1be2ccc1b3
publdaze <publdaze@naver.com>
2023-10-11 22:37:26 +0900

    Feat: 페이지 조회 더미 데이터 추가 및 기존 dto 안 맞는 부분 수정
    #69

--- src/dummy/page.ts
+++ src/dummy/page.ts
@@ -1,9 +1,10 @@
 interface Page {
   pageId: number;
   pageName: string;
+  content: string;
 }
 
-export const recentChangePageDummyData: Page[] = [
+export const recentChangePageDummyData: Omit<Page, 'content'>[] = [
   { pageId: 1, pageName: '최근 변경 페이지 테스트' },
   { pageId: 2, pageName: '페이지 이름2' },
   { pageId: 3, pageName: '페이지 이름3' },
@@ -11,24 +12,58 @@ export const recentChangePageDummyData: Page[] = [
 
 interface Post {
   postId: number;
-  postName: string;
+  postTitle: string;
   content: string;
+  index: string;
 }
 
-export const postDummyData: Post[] = [
+export const pageDummyData: Page[] = [
   {
-    postId: 1,
-    postName: '테스트 게시글',
+    pageId: 1,
+    pageName: '테스트 게시글',
     content: '테스트 게시글 내용',
   },
   {
-    postId: 2,
-    postName: '테스트 게시글2',
+    pageId: 2,
+    pageName: '테스트 게시글2',
     content: '테스트 게시글 내용2',
   },
   {
-    postId: 3,
-    postName: '테스트 게시글3',
+    pageId: 3,
+    pageName: '테스트 게시글3',
     content: '테스트 게시글 내용3',
   },
 ];
+
+interface PageInfo {
+  pageName: string;
+  postList: Post[];
+  goodCount: number;
+  badCount: number;
+}
+
+export const pageInfo: PageInfo = {
+  pageName: '부산대학교',
+  postList: [
+    {
+      postId: 1,
+      index: '1',
+      postTitle: '개요',
+      content: '내용...',
+    },
+    {
+      postId: 2,
+      index: '2',
+      postTitle: '역사',
+      content: '내용...',
+    },
+    {
+      postId: 4,
+      index: '2-1',
+      postTitle: '컴공의 역사',
+      content: '내용...',
+    },
+  ],
+  goodCount: 7326,
+  badCount: 545,
+};
