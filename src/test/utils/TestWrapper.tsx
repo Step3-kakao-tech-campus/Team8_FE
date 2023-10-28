42654970a45f7dcecbd6f71b683db1421f232f85
publdaze <publdaze@naver.com>
2023-10-28 18:09:03 +0900

    Test: 테스트 코드 Wrapper query client provider 추가
    #24

--- src/test/utils/TestWrapper.tsx
+++ src/test/utils/TestWrapper.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable import/prefer-default-export */
+import { queryClient } from '@apis/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 const TestWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </BrowserRouter>
+  );
 };
 
 export default TestWrapper;
