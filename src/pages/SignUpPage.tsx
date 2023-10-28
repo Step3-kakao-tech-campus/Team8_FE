8aafe6b41e5df55006bc159cc597297616cf8f4c
publdaze <publdaze@naver.com>
2023-10-28 17:09:52 +0900

    Feat: 회원가입 API 연결
    #24

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { Button, Input } from '@material-tailwind/react';
 import { ReactComponent as TextLogo } from '@assets/images/logo/textLogo.svg';
 import { EMAIL_PATTERN, NAME_PATTERN, PASSWORD_PATTERN } from '@constants/validationPatterns';
 import { REQUIRE_ERROR_MSG } from '@constants/errorMsg';
+import { useMutation } from '@tanstack/react-query';
+import { signUpFn } from '@apis/authApi';
+import { useNavigate } from 'react-router-dom';
 
 interface SignUpInputs {
   email: string;
@@ -14,14 +17,29 @@ interface SignUpInputs {
 const SignUpPage = () => {
   const {
     register,
+    handleSubmit,
     formState: { errors, isValid },
   } = useForm<SignUpInputs>({ mode: 'onChange' });
+  const navigate = useNavigate();
+
+  const { mutate: signUp } = useMutation({ mutationFn: signUpFn });
+
+  const handleSignUpSubmit: SubmitHandler<FieldValues> = ({ email, password, name }) => {
+    signUp(
+      { email, password, name },
+      {
+        onSuccess: () => {
+          navigate('/login');
+        },
+      },
+    );
+  };
 
   return (
     <div className='flex min-h-full flex-col justify-center px-8'>
       <div className='mx-auto space-y-4 w-full shadow max-w-[450px] px-16 pt-9 pb-16'>
         <TextLogo className='w-36 m-auto mb-8' data-testid='textLogo' />
-        <form className='space-y-12'>
+        <form className='space-y-12' onSubmit={handleSubmit(handleSignUpSubmit)}>
           <div>
             <Input
               label='이메일'
@@ -62,7 +80,7 @@ const SignUpPage = () => {
             />
             {errors.name && <p className='text-xs mt-1 mx-1 flex items-center text-error'>{errors.name.message}</p>}
           </div>
-          <Button className='w-full' data-testid='signUpBtn' disabled={!isValid}>
+          <Button type='submit' className='w-full' data-testid='signUpBtn' disabled={!isValid}>
             회원가입
           </Button>
         </form>
