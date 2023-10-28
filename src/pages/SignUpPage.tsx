6fe27a150ffc771196db3af96d0f4f30a15e4758
publdaze <publdaze@naver.com>
2023-10-28 16:23:37 +0900

    Feat: 회원가입 onChange 시 유효성 에러 검증 및 메시지 출력
    #24

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -12,42 +12,56 @@ interface SignUpInputs {
 }
 
 const SignUpPage = () => {
-  const { register } = useForm<SignUpInputs>();
+  const {
+    register,
+    formState: { errors },
+  } = useForm<SignUpInputs>({ mode: 'onChange' });
 
   return (
     <div className='flex min-h-full flex-col justify-center px-8'>
       <div className='mx-auto space-y-4 w-full shadow max-w-[450px] px-16 pt-9 pb-16'>
         <TextLogo className='w-36 m-auto mb-8' data-testid='textLogo' />
         <form className='space-y-12'>
-          <Input
-            label='이메일'
-            type='email'
-            crossOrigin=''
-            data-testid='email'
-            {...register('email', {
-              required: REQUIRE_ERROR_MSG,
-              pattern: { value: EMAIL_PATTERN, message: '이메일 주소를 다시 확인해주세요.' },
-            })}
-          />
-          <Input
-            label='비밀번호'
-            type='password'
-            crossOrigin=''
-            data-testid='password'
-            {...register('password', {
-              required: REQUIRE_ERROR_MSG,
-              pattern: { value: PASSWORD_PATTERN, message: '8~20자 영문과 숫자를 사용하세요.' },
-            })}
-          />
-          <Input
-            label='이름'
-            crossOrigin=''
-            data-testid='name'
-            {...register('name', {
-              required: REQUIRE_ERROR_MSG,
-              pattern: { value: NAME_PATTERN, message: '1~10자 한글, 영어만 가능합니다.' },
-            })}
-          />
+          <div>
+            <Input
+              label='이메일'
+              type='email'
+              crossOrigin=''
+              data-testid='email'
+              {...register('email', {
+                required: REQUIRE_ERROR_MSG,
+                pattern: { value: EMAIL_PATTERN, message: '이메일 형식이 맞지 않습니다.' },
+              })}
+            />
+            {errors.email && <p className='text-xs mt-1 mx-1 flex items-center text-error'>{errors.email.message}</p>}
+          </div>
+          <div>
+            <Input
+              label='비밀번호'
+              type='password'
+              crossOrigin=''
+              data-testid='password'
+              {...register('password', {
+                required: REQUIRE_ERROR_MSG,
+                pattern: { value: PASSWORD_PATTERN, message: '8~20자 영문과 숫자를 사용하세요.' },
+              })}
+            />
+            {errors.password && (
+              <p className='text-xs mt-1 mx-1 flex items-center text-error'>{errors.password.message}</p>
+            )}
+          </div>
+          <div>
+            <Input
+              label='이름'
+              crossOrigin=''
+              data-testid='name'
+              {...register('name', {
+                required: REQUIRE_ERROR_MSG,
+                pattern: { value: NAME_PATTERN, message: '1~10자 한글, 영어만 가능합니다.' },
+              })}
+            />
+            {errors.name && <p className='text-xs mt-1 mx-1 flex items-center text-error'>{errors.name.message}</p>}
+          </div>
           <Button className='w-full' data-testid='signUpBtn'>
             회원가입
           </Button>
