57d077a76abed3524f8aa6272bf5ffa03b7410ab
publdaze <publdaze@naver.com>
2023-10-28 17:50:15 +0900

    Test: 라우터 관련 테스트 코드 에러 해결
    #24

--- src/test/pages/SignUpPage.test.tsx
+++ src/test/pages/SignUpPage.test.tsx
@@ -1,35 +1,56 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import SignUpPage from '@pages/SignUpPage';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('회원가입', () => {
   describe('UI 컴포넌트 렌더링', () => {
     it('텍스트 로고 렌더링 성공', () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>,
+      );
       const textLogoElement = screen.getByTestId('textLogo');
 
       expect(textLogoElement).toBeInTheDocument();
     });
     it('이메일 인풋 렌더링 성공', () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>,
+      );
       const emailInputElement = screen.getByTestId('email');
 
       expect(emailInputElement).toBeInTheDocument();
     });
     it('비밀번호 인풋 렌더링 성공', () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>,
+      );
       const passwordInputElement = screen.getByTestId('password');
 
       expect(passwordInputElement).toBeInTheDocument();
     });
     it('이름 인풋 렌더링 성공', () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>,
+      );
       const nameInputElement = screen.getByTestId('name');
 
       expect(nameInputElement).toBeInTheDocument();
     });
     it('회원가입 버튼 렌더링 성공', () => {
-      render(<SignUpPage />);
+      render(
+        <BrowserRouter>
+          <SignUpPage />
+        </BrowserRouter>,
+      );
       const signUpBtnInputElement = screen.getByTestId('signUpBtn');
 
       expect(signUpBtnInputElement).toBeInTheDocument();
