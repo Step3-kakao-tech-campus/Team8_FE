e3852bc7a914fa3bb300dcf7f1b96bd6d313b149
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-10-31 20:23:50 +0900

    Feat: 이미지 선택 여부에 따른 버튼 동작 처리
    - 이미지가 선택되지 않은 상태에서 "확인"버튼 누르면 안넘어감
    - 이미지가 선택된 상태에서 "다음에 하기"버튼 누르면 안넘어감

--- src/components/GroupCreate/GroupCreatePhotoSection.tsx
+++ src/components/GroupCreate/GroupCreatePhotoSection.tsx
@@ -12,6 +12,7 @@ const GroupCreatePhotoSection = ({ onNextStep }: onNextStepProps) => {
   const setGroupInfo = useSetRecoilState(groupCreateInfoState);
   const inputRef = useRef<HTMLInputElement>(null);
   const [imgPreview, setImgPreview] = useState<string>('');
+  const [error, setError] = useState<string>('');
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files?.length) return;
@@ -24,16 +25,28 @@ const GroupCreatePhotoSection = ({ onNextStep }: onNextStepProps) => {
     fileReader.onload = (event) => {
       const result = event?.target?.result as string;
       setImgPreview(result);
+      setError('');
     };
   };
   const handleFileRemove = () => {
     setImgPreview('');
+    setError('');
   };
   const handleUploadClick = () => {
     inputRef.current?.click();
   };
+  const handleSkip = () => {
+    if (imgPreview) {
+      setError('이미지를 삭제한 후');
+      return;
+    }
+    onNextStep();
+  };
   const handleNextStep = () => {
-    // TODO: 사진 조건 체크(있는지 없는지)
+    if (!imgPreview) {
+      setError('이미지를 선택하거나');
+      return;
+    }
     onNextStep();
   };
   return (
@@ -89,16 +102,23 @@ const GroupCreatePhotoSection = ({ onNextStep }: onNextStepProps) => {
           <p className='mt-2 text-sm font-semibold text-gray-600'>이미지 선택하기</p>
         </Button>
       )}
+      {error && (
+        <p className='text-right !mt-2 text-sm text-red-600'>
+          {error}, <span className='font-bold'>[다음에 하기]</span>버튼을 클릭해주세요.
+        </p>
+      )}
       <div className='flex justify-between items-center'>
         <Button
           variant='text'
           ripple={false}
           className='p-2 text-sm font-normal text-gray-500 hover:text-gray-600 hover:bg-transparent active:bg-transparent underline underline-offset-2'
-          onClick={handleNextStep}
+          onClick={handleSkip}
         >
           다음에 하기
         </Button>
-        <Button onClick={handleNextStep}>확인</Button>
+        <Button ripple={false} onClick={handleNextStep}>
+          확인
+        </Button>
       </div>
     </section>
   );
