ce06e6da32264008eb81795436ed660817816032
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-10-31 23:06:51 +0900

    Feat: 그룹생성 1단계 유효성 검사 및 상태 관리
    - 다음 단계로 갔다가 돌아와도 유지되게 상태 관리

--- src/constants/errorMsg.ts
+++ src/constants/errorMsg.ts
@@ -3,3 +3,4 @@ export const EMAIL_ERROR_MSG = '이메일 형식이 맞지 않습니다.';
 export const PASSWORD_ERROR_MSG = '8~20자 영문과 숫자를 사용하세요.';
 export const PASSWORD_CONFIRM_ERROR_MSG = '비밀번호가 일치하지 않습니다.';
 export const NAME_ERROR_MSG = '1~10자 한글, 영어만 가능합니다.';
+export const GROUP_NAME_ERROR_MSG = '그룹 이름은 최소 2자, 최대 12자까지 가능합니다.';
