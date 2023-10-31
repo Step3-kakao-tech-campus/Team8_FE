6816f87b875a07c9d91a5c8dbc9bb2d710e9ccf2
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-10-31 22:35:52 +0900

    Feat: 그룹생성 3단계

--- src/recoil/atoms/group.ts
+++ src/recoil/atoms/group.ts
@@ -4,6 +4,7 @@ import { GROUP_CREATE_RECOIL_KEYS } from '@constants/recoilKeys';
 const groupCreateInfoState = atom({
   key: GROUP_CREATE_RECOIL_KEYS.groupInfo,
   default: {
+    groupType: 'UNOFFICIAL_OPENED' as 'UNOFFICIAL_OPENED' | 'UNOFFICIAL_CLOSED',
     groupName: '' as string,
     groupImage: undefined as File | undefined,
     groupNickname: '' as string,
