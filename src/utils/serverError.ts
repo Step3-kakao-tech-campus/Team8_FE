903d1f108360a36abf8aa3dee9bcea95ebda3b2c
publdaze <publdaze@naver.com>
2023-11-01 20:36:43 +0900

    Feat: api 에러 반환 타입 변경된 거 반영

--- src/utils/serverError.ts
+++ src/utils/serverError.ts
@@ -3,13 +3,13 @@ import axios from 'axios';
 interface ServerErrorData {
   success: boolean;
   response: unknown;
-  error: string;
+  error: { message: string; status: number };
 }
 
 // eslint-disable-next-line import/prefer-default-export
 export const getErrorMsg = (serverError: unknown) => {
   if (axios.isAxiosError<ServerErrorData>(serverError)) {
-    return serverError.response?.data.error;
+    return serverError.response?.data.error.message;
   }
   return null;
 };
