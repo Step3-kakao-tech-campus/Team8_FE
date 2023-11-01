8e55223baf1d6b087b0456d555596de2464132c1
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-11-01 17:24:54 +0900

    Feat: 그룹생성 stepper를 이용한 단계이동 막기
    - stepper로 단계이동 불가능, 따라서 이전 단계버튼을 확인버튼과 가깝게 이동

--- src/pages/GroupCreatePage.tsx
+++ src/pages/GroupCreatePage.tsx
@@ -24,27 +24,28 @@ const GroupCreatePage = () => {
 
   return (
     <div className='flex flex-col pt-10 max-w-xl mx-auto mb-20 min-h-[500px]'>
-      <Button
-        variant='text'
-        className='p-0 mb-2 max-w-fit text-gray-500'
-        onClick={() => setCurrentStep((currentStep - 1) as 1 | 2 | 3 | 4 | 5)}
-      >
-        <div className='flex gap-0 items-center max-h-fit'>
-          <MdKeyboardArrowLeft size={15} />
-          <span className='text-xs pr-2'>이전 단계로</span>
-        </div>
-      </Button>
-      <Stepper className='w-40 mb-14 cursor-pointer' activeStep={currentStep - 1}>
-        {Object.keys(groupCreateSections).map((step) => (
-          <Step
-            key={uuidv4()}
-            className='w-6 h-6 text-xs bg-gray-100'
-            onClick={() => setCurrentStep(parseInt(step, 10) as 1 | 2 | 3 | 4 | 5)}
+      <div className='flex justify-between gap-4 mb-8'>
+        <Stepper className='w-40 mb-4 cursor-pointer' activeStep={currentStep - 1}>
+          {Object.keys(groupCreateSections).map((step) => (
+            <Step key={uuidv4()} className='w-6 h-6 text-xs bg-gray-100'>
+              {step}
+            </Step>
+          ))}
+        </Stepper>
+        {currentStep !== 5 && (
+          <Button
+            variant='text'
+            ripple={false}
+            className='p-0 mb-2 max-w-fit text-gray-500 hover:bg-transparent active:bg-transparent'
+            onClick={() => setCurrentStep((currentStep > 1 ? currentStep - 1 : currentStep) as 1 | 2 | 3 | 4 | 5)}
           >
-            {step}
-          </Step>
-        ))}
-      </Stepper>
+            <div className='flex gap-0 items-center max-h-fit'>
+              <MdKeyboardArrowLeft size={15} />
+              <span className='text-xs pr-2'>이전 단계로</span>
+            </div>
+          </Button>
+        )}
+      </div>
       {groupCreateSections[currentStep]}
     </div>
   );
