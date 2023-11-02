56adf6f4be70ece63448de72974d90a0631c5410
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-11-02 20:04:04 +0900

    Feat: 첫글 작성
    - 페이지에 아무 글이 없을 때 새로 작성하는 경우
    
    #131

--- src/pages/GroupMainPage.tsx
+++ src/pages/GroupMainPage.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { MdArrowCircleRight } from 'react-icons/md';
 import { v4 as uuidv4 } from 'uuid';
@@ -21,16 +21,6 @@ interface Post {
   parentPostId: number;
 }
 
-interface Error {
-  response: {
-    data: {
-      error: {
-        message: string;
-      };
-    };
-  };
-}
-
 const GroupMainPage = () => {
   const navigate = useNavigate();
   const { groupId, page } = useParams();
@@ -39,9 +29,12 @@ const GroupMainPage = () => {
   if (!groupId) return null;
   if (!page) return null;
 
-  const { data, error, status } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: PAGE_KEYS.byTitle({ groupId: numGroupId, title: page }),
     queryFn: () => getPageByTitleFn({ groupId: numGroupId, title: page }),
+    onError: () => {
+      navigate('/404');
+    },
   });
 
   const { pageName, pageId, postList, goodCount, badCount } = data?.data?.response || {
@@ -52,18 +45,11 @@ const GroupMainPage = () => {
     badCount: 0,
   };
 
-  const handleWriteClick = () => {
+  const handleNewPostClick = () => {
     navigate('개요/edit', {
-      state: { pageId, index: '1.', pageName, postTitle: '개요', content: '' },
+      state: { pageId, parentPostId: 0, order: 1, index: '1.', pageName, postTitle: '개요', content: '', type: 'new' },
     });
   };
-
-  useEffect(() => {
-    if (error) console.log(error);
-    if (error && (error as Error).response.data.error.message === '존재하지 않는 페이지 입니다.') {
-      navigate('/404', { replace: true });
-    }
-  }, [data, error, status]);
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -80,39 +66,43 @@ const GroupMainPage = () => {
             />
           }
         />
-        <PageContainer pageId={postList.length !== 0 ? pageId : undefined} hasRecentChangeList>
-          {postList.length !== 0 ? (
-            <ul>
-              {postList.map((post: Post) => (
-                <li key={uuidv4()}>
-                  <Post
-                    pageId={pageId}
-                    pageName={pageName}
-                    index={post.index}
-                    postTitle={post.postTitle}
-                    content={post.content}
-                  />
-                  <AddPostButton />
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <article className='flex justify-between items-center p-4 bg-gray-100 rounded-lg px-4'>
-              <p className='text-sm shrink-0'>
-                <span className='font-bold'>{`"${page}"`}</span>에 새로운 글을 작성해보세요!
-              </p>
-              <Button
-                variant='text'
-                ripple={false}
-                className='group flex items-center gap-1 py-1 px-2 text-sm font-bold hover:bg-transparent active:bg-transparent shrink-0'
-                onClick={handleWriteClick}
-              >
-                <span>글쓰기</span>
-                <MdArrowCircleRight className='w-5 h-5 group-hover:animate-arrowBounce' />
-              </Button>
-            </article>
-          )}
-        </PageContainer>
+        {isLoading ? (
+          <p>loading</p>
+        ) : (
+          <PageContainer pageId={postList.length !== 0 ? pageId : undefined} hasRecentChangeList>
+            {postList.length !== 0 ? (
+              <ul>
+                {postList.map((post: Post) => (
+                  <li key={uuidv4()}>
+                    <Post
+                      pageId={pageId}
+                      pageName={pageName}
+                      index={post.index}
+                      postTitle={post.postTitle}
+                      content={post.content}
+                    />
+                    <AddPostButton />
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <article className='flex justify-between items-center p-4 bg-gray-100 rounded-lg px-4'>
+                <p className='text-sm shrink-0'>
+                  <span className='font-bold'>{`"${page}"`}</span>에 새로운 글을 작성해보세요!
+                </p>
+                <Button
+                  variant='text'
+                  ripple={false}
+                  className='group flex items-center gap-1 py-1 px-2 text-sm font-bold hover:bg-transparent active:bg-transparent shrink-0'
+                  onClick={handleNewPostClick}
+                >
+                  <span>글쓰기</span>
+                  <MdArrowCircleRight className='w-5 h-5 group-hover:animate-arrowBounce' />
+                </Button>
+              </article>
+            )}
+          </PageContainer>
+        )}
       </div>
     </Suspense>
   );
