3d04024140d1dd9bf28c449051e69224d9bb7f40
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-11-07 18:00:51 +0900

    Fix: 머지 충돌 해결
    #169 #64

--- src/apis/pageApi.ts
+++ src/apis/pageApi.ts
@@ -2,28 +2,28 @@ import { instance } from './axios';
 
 // page 관련 api
 
-interface PageInfoType {
+interface PageInfo {
   groupId: number;
   pageId?: number;
   title?: string;
   pageName?: string;
 }
 
-export const getPageByTitleFn = ({ groupId, title }: PageInfoType) => instance.get(`/group/${groupId}/page?title=${title}`);
+export const getPageByTitleFn = ({ groupId, title }: PageInfo) => instance.get(`/group/${groupId}/page?title=${title}`);
 
-export const createPageFn = ({ groupId, pageName }: PageInfoType) =>
+export const createPageFn = ({ groupId, pageName }: PageInfo) =>
   instance.post(`/group/${groupId}/page/create`, { pageName });
 
-export const getRecentChangeListFn = ({ groupId }: PageInfoType) => instance.get(`/group/${groupId}/page/recent`);
+export const getRecentChangeListFn = ({ groupId }: PageInfo) => instance.get(`/group/${groupId}/page/recent`);
 
-export const pageLikeFn = ({ groupId, pageId }: PageInfoType) => instance.post(`/group/${groupId}/page/${pageId}/like`);
+export const pageLikeFn = ({ groupId, pageId }: PageInfo) => instance.post(`/group/${groupId}/page/${pageId}/like`);
 
-export const pageHateFn = ({ groupId, pageId }: PageInfoType) => instance.post(`/group/${groupId}/page/${pageId}/hate`);
+export const pageHateFn = ({ groupId, pageId }: PageInfo) => instance.post(`/group/${groupId}/page/${pageId}/hate`);
 
 export const searchPageFn = ({ groupId, keyword }: { groupId: number; keyword: string }) =>
   instance.get(`/group/${groupId}/page/search?keyword=${keyword}&page=1`);
 
-export const getIndexListFn = ({ groupId, pageId }: PageInfoType) => instance.get(`/group/${groupId}/page/${pageId}/index`);
+export const getIndexListFn = ({ groupId, pageId }: PageInfo) => instance.get(`/group/${groupId}/page/${pageId}/index`);
 
-export const checkPageExistence = ({ groupId, title }: PageInfoType) =>
+export const checkPageExistence = ({ groupId, title }: PageInfo) =>
   instance.get(`/group/${groupId}/page/link?title=${title}`);
