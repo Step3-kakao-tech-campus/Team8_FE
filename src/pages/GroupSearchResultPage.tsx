74bfae73d03af41ae5b1e70fdb164779ae7510fd
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-11-09 20:26:23 +0900

    Remove: 공식 그룹 만들러가기 버튼 삭제
    #182 #183

--- src/pages/GroupSearchResultPage.tsx
+++ src/pages/GroupSearchResultPage.tsx
@@ -49,10 +49,12 @@ const GroupSearchResultPage = () => {
       {groupData.length === 0 ? (
         <div className='flex flex-col items-center justify-center h-[300px]'>
           <h1 className='mb-2 text-xl font-bold'>관련된 그룹이 없어요.</h1>
-          <Link to='/groupCreate' className='mt-2 text-sm text-gray-400 hover:text-blue-300'>
-            <span>새로운 그룹 만들러 가기</span>
-            <MdChevronRight className='inline-block' width={15} height={15} />
-          </Link>
+          {!isOfficialGroup && (
+            <Link to='/groupCreate' className='mt-2 text-sm text-gray-400 hover:text-blue-300'>
+              <span>새로운 그룹 만들러 가기</span>
+              <MdChevronRight className='inline-block' width={15} height={15} />
+            </Link>
+          )}
         </div>
       ) : (
         <GroupList groups={groupData} />
