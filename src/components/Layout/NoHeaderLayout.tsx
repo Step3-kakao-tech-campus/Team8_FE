92bf282785de4614da2447522b99bae2cad2bad7
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-11-11 11:27:04 +0900

    Feat: 페이지 이동 시 맨 위로 스크롤
    - MainLayout, NoHeaderLayout 에만 적용

--- src/components/Layout/NoHeaderLayout.tsx
+++ src/components/Layout/NoHeaderLayout.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ErrorBoundary from '@components/Error/ErrorBoundary';
 import ErrorFallback from '@components/Error/ErrorFallback';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '@components/Common/ScrollToTop';
 
 const NoHeaderLayout = () => {
   return (
     <ErrorBoundary fallback={ErrorFallback}>
+      <ScrollToTop />
       <div className='max-w-3xl min-w-max mx-auto mt-[59px] pt-20 pb-12'>
         <Outlet />
       </div>
