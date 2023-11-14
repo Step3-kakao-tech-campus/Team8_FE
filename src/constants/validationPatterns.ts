17b9712e556c69289d3b3cb617e60778a556f69a
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-11-15 00:42:29 +0900

    Feat: 그룹 생성 시 그룹 이름 패턴 추가

--- src/constants/validationPatterns.ts
+++ src/constants/validationPatterns.ts
@@ -1,5 +1,6 @@
 export const EMAIL_PATTERN = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 export const PASSWORD_PATTERN = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/;
 export const NAME_PATTERN = /^[가-힣a-zA-Z]{1,10}$/;
+export const GROUP_NAME_PATTERN = /^[a-zA-Z가-힣0-9\s]*$/;
 export const GROUP_NICKNAME_PATTERN = /^[가-힣a-zA-Z0-9]{2,8}$/;
 export const PNU_MAIL_PATTERN = /[@]pusan\.ac\.kr$/;
